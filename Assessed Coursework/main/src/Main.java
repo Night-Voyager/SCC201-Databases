0aabfbf5a35f7500d44521de191f16e591b7e4ff
Night-Voyager <18722007@bjtu.edu.cn>
2021-06-15 08:25:07 +0800

    feat: add interaction in console

--- Assessed Coursework/main/src/Main.java
+++ Assessed Coursework/main/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        DbAnswer dbAnswer = new DbAnswer("Northwind.db");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the filename of the database (e.g., University.db).");
+        String dbName = in.next();
+
+        DbAnswer dbAnswer = new DbAnswer(dbName);
         dbAnswer.go();
     }
 }
